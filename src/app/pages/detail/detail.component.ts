dd3d483c0666a3b61436eb03a402a10922b10d09
BJaviersito <losthyghost@gmail.com>
2024-10-30 15:41:25 -0600

    add button to go to the principal page

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -7,11 +7,12 @@ import { KeyValuePipe, NgFor, NgIf } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AsyncPipe } from '@angular/common';
 import { MarketNamePipe } from '../../pipes/market-name.pipe';
+import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [NgIf, HttpClientModule, AsyncPipe, NgFor, KeyValuePipe, MarketNamePipe],
+  imports: [NgIf, HttpClientModule, AsyncPipe, NgFor, KeyValuePipe, MarketNamePipe,RouterLinkActive, RouterLink],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css',
   providers: [CardService]
